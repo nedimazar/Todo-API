dabd8cf4b0366d431f289206a216e160a32ef04d
nedimazar <nedimazar@hotmail.com>
2020-09-04 17:20:41 +0300

    Using REST to test HTTP requests.

--- index.js
+++ index.js
@@ -1,25 +1,32 @@
-const express = require('express')
-const Sequelize = require('sequelize')
+import Express from "express";
+import Sequelize from "sequelize";
 
-const sequelize = new Sequelize('postgres://localhost:5432/nedimazar')
+const sequelize = new Sequelize('postgres://localhost:5432/nedimazar');
 
-const app = express()
-
-const port = 3000
-
-app.use(express.json());
-
-app.get('/', (req, res) => res.json({ message: 'Hello World' }))
-
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+const app = Express();
 
 
-sequelize.authenticate().then(() => {
 
-console.log('Connection has been established successfully.');
+const port = 3000;
 
-}).catch(err => {
-
-console.error('Unable to connect to the database:', err);
-
+app.get("/", (req, res) => {
+    res.send("Hello World!");
 });
+
+app.listen(port, () => console.log("Listening on port " + port));
+// app.use(Express.json());
+
+// app.get('/', (req, res) => res.json({ message: 'Hello World' }))
+
+// app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+
+// sequelize.authenticate().then(() => {
+
+// console.log('Connection has been established successfully.');
+
+// }).catch(err => {
+
+// console.error('Unable to connect to the database:', err);
+
+// });
