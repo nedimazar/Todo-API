42a2e2ad66f570e0b4fcbed88d5b464f7af50920
nedimazar <nedimazar@hotmail.com>
2020-09-05 16:13:43 +0300

    Formatting.

--- index.js
+++ index.js
@@ -20,17 +20,11 @@ app.get('/', (req, res) => res.json({
 
 app.listen(port, () => console.log(`Listening on: http://localhost:${port}`))
 
-
 sequelize.authenticate().then(() => {
-
   console.log('Connection has been established successfully.');
-
 }).catch(err => {
-
   console.error('Unable to connect to the database:', err);
-
 });
-
 
 class todo extends Model {}
 
@@ -38,8 +32,8 @@ todo.init({
   // Model attributes are defined here
   id: {
     type: DataTypes.BIGINT,
-    primaryKey: true
-    // allowNull defaults to true
+    primaryKey: true,
+    allowNull: false
   },
   message: {
     type: DataTypes.STRING,
