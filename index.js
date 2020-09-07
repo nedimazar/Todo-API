48d1ae3b632d05295a2d3b3153efba3b1c8c322a
Jake Ols <jake@kurtnoble.com>
2020-09-07 15:12:55 -0700

    add notes, working code

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const {
 
 const bodyParser = require("body-parser");
 
-const sequelize = new Sequelize('postgres://localhost:5433/todoapi');
+const sequelize = new Sequelize('postgres://localhost:5432/todoapi');
 
 sequelize.authenticate().then(() => {
   console.log('Connection has been established successfully.');
@@ -31,11 +31,12 @@ app.listen(port, () => console.log(`Listening on: http://localhost:${port}`))
 class todo extends Model {}
 
 todo.init({
-  id: {
-    type: DataTypes.BIGINT,
-    primaryKey: true,
-    allowNull: false
-  },
+  // @NED - it's best to just allow sequelize to handle these fields automatically for you
+  // id: {
+  //   type: DataTypes.BIGINT,
+  //   primaryKey: true,
+  //   allowNull: false
+  // },
   message: {
     type: DataTypes.STRING,
     allowNull: false
@@ -69,17 +70,23 @@ app.get("/api/todos/:todoId", (req, res) => {
 });
 
 // This is broken, message and id are always null
-app.post('/', (req, res) => {
-  console.log("\n\n\n\n" + (req.body.message, req.body.id) + "\n\n\n\n")
+app.post('/api/todos', (req, res) => { // @NED - assuming you want it at this route
+  console.log("\n\n\n\n" + (req.body) + "\n\n\n\n")
   const todoItem = {
     isComplete: false,
     message: req.body.message,
-    id: req.body.id,
+    // id: req.body.id, @NED -  don't need to set the ID here, 
+    // sequelize will do it for you (unless you're trying to add the todolist ID?)
   };
 
   todo.create(todoItem)
     .then((data) => {
       res.send(data);
+    })
+    .catch((err) => {
+      res.status(400).send({
+        message: err,
+      });
     })
 });
 
