5932bfa2e4144026e6b3c9796608864e1e11b0a0
nedimazar <nedimazar@hotmail.com>
2020-09-08 01:00:08 +0300

    Bug in creating new list item.

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const {
   Model
 } = require("sequelize");
 
+const bodyParser = require("body-parser");
+
 const sequelize = new Sequelize('postgres://localhost:5433/todoapi');
 
 sequelize.authenticate().then(() => {
@@ -17,6 +19,10 @@ sequelize.authenticate().then(() => {
 const app = Express();
 
 const port = 3000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
 
 app.use(Express.json());
 
@@ -33,12 +39,17 @@ todo.init({
   message: {
     type: DataTypes.STRING,
     allowNull: false
+  },
+  isComplete: {
+    type: DataTypes.BOOLEAN,
+    allowNull: false
   }
 }, {
   // Other model options go here
   sequelize, // We need to pass the connection instance
   modelName: 'todo' // We need to choose the model name
 });
+
 
 todo.sync({
   //force: true
@@ -50,10 +61,26 @@ app.get('/', (req, res) => res.json({
 
 // This lets us query the database by the id of a TODO item.
 app.get("/api/todos/:todoId", (req, res) => {
+  console.log(req)
   let reqId = req.params.todoId;
   todo.findOne({where: {id: reqId}}).then((todo) => {
     res.json(todo);
   });
+});
+
+// This is broken, message and id are always null
+app.post('/', (req, res) => {
+  console.log("\n\n\n\n" + (req.body.message, req.body.id) + "\n\n\n\n")
+  const todoItem = {
+    isComplete: false,
+    message: req.body.message,
+    id: req.body.id,
+  };
+
+  todo.create(todoItem)
+    .then((data) => {
+      res.send(data);
+    })
 });
 
 
